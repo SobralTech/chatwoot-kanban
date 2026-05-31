53f15fe74efb0e414e879fcb3d6c1f0a07bbc299
Pedro Henrique <pedro88rising@gmail.com>
2026-05-31 10:27:35 -0300

    feat(kanban): add opportunity card policy

---
+++ app/policies/kanban_card_policy.rb
@@ -0,0 +1,77 @@
+class KanbanCardPolicy < ApplicationPolicy
+  def show?
+    valid_card_scope? && card_access?
+  end
+
+  def create?
+    show?
+  end
+
+  def update?
+    show?
+  end
+
+  def destroy?
+    show?
+  end
+
+  def reorder?
+    show?
+  end
+
+  private
+
+  def valid_card_scope?
+    card_account? && board_account? && stage_account? && stage_board? && contact_account? && inbox_account?
+  end
+
+  def card_account?
+    record.account_id == account&.id
+  end
+
+  def board_account?
+    record.kanban_board&.account_id == account&.id
+  end
+
+  def stage_account?
+    record.kanban_stage&.account_id == account&.id
+  end
+
+  def stage_board?
+    record.kanban_stage&.kanban_board_id == record.kanban_board_id
+  end
+
+  def contact_account?
+    record.contact&.account_id == account&.id
+  end
+
+  def inbox_account?
+    record.inbox&.account_id == account&.id
+  end
+
+  def card_access?
+    record.conversation_id.present? ? conversation_card_access? : manual_card_access?
+  end
+
+  def conversation_card_access?
+    valid_conversation_scope? && ConversationPolicy.new(user_context, record.conversation).show?
+  end
+
+  def valid_conversation_scope?
+    record.conversation&.account_id == account&.id &&
+      record.conversation&.contact_id == record.contact_id &&
+      record.conversation&.inbox_id == record.inbox_id
+  end
+
+  def manual_card_access?
+    administrator? || inbox_access?
+  end
+
+  def administrator?
+    account_user&.administrator?
+  end
+
+  def inbox_access?
+    user.inboxes.where(account_id: account&.id).exists?(id: record.inbox_id)
+  end
+end
