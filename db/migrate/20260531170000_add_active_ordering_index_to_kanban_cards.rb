18fc4d1a2267570b13cb42e1ee14cba230be5f17
Pedro Henrique <pedro88rising@gmail.com>
2026-06-03 22:25:47 -0300

    fix(kanban): make card index migrations concurrent

--- db/migrate/20260531170000_add_active_ordering_index_to_kanban_cards.rb
+++ db/migrate/20260531170000_add_active_ordering_index_to_kanban_cards.rb
@@ -1,8 +1,15 @@
 class AddActiveOrderingIndexToKanbanCards < ActiveRecord::Migration[7.1]
-  def change
+  disable_ddl_transaction!
+
+  def up
     add_index :kanban_cards,
               [:kanban_board_id, :kanban_stage_id, :position, :created_at, :id],
               where: 'active = true',
-              name: 'index_active_kanban_cards_on_board_stage_order'
+              name: 'index_active_kanban_cards_on_board_stage_order',
+              algorithm: :concurrently
+  end
+
+  def down
+    remove_index :kanban_cards, name: 'index_active_kanban_cards_on_board_stage_order', algorithm: :concurrently
   end
 end
