ab1f24663431aca6390531902d4662980b434f31
Pedro Henrique <desenvolvimento.sobraltec@gmail.com>
2026-06-20 11:26:35 -0300

    chore: sync model annotations and Gemfile.lock with current schema/deps
    Account and KanbanCardAssignee annotations were stale relative to
    schema.rb. Gemfile.lock picks up resolved patch/minor bumps for
    parser and scout_apm.

--- app/models/kanban_card_assignee.rb
+++ app/models/kanban_card_assignee.rb
@@ -1,3 +1,27 @@
+# == Schema Information
+#
+# Table name: kanban_card_assignees
+#
+#  id             :bigint           not null, primary key
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  account_id     :bigint           not null
+#  kanban_card_id :bigint           not null
+#  user_id        :bigint           not null
+#
+# Indexes
+#
+#  index_kanban_card_assignees_on_account_id                  (account_id)
+#  index_kanban_card_assignees_on_kanban_card_id              (kanban_card_id)
+#  index_kanban_card_assignees_on_kanban_card_id_and_user_id  (kanban_card_id,user_id) UNIQUE
+#  index_kanban_card_assignees_on_user_id                     (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (account_id => accounts.id)
+#  fk_rails_...  (kanban_card_id => kanban_cards.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class KanbanCardAssignee < ApplicationRecord
   belongs_to :account
   belongs_to :kanban_card
