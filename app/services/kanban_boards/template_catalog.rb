fbf6f7014068b46aed908c518db5034e26cf1977
SobralTec Desenvolvimento <sobraltec82@gmail.com>
2026-08-15 10:29:12 -0300

    refactor(kanban): compact template service namespaces

--- app/services/kanban_boards/template_catalog.rb
+++ app/services/kanban_boards/template_catalog.rb
@@ -1,25 +1,23 @@
-module KanbanBoards
-  module TemplateCatalog
-    TEMPLATES = {
-      'blank' => {
-        stages: [{ name_key: 'entry', color: '#8B8D98' }],
-        won: { name_key: 'won' },
-        lost: { name_key: 'lost' },
-        lost_reasons: []
-      }
-    }.freeze
+module KanbanBoards::TemplateCatalog
+  TEMPLATES = {
+    'blank' => {
+      stages: [{ name_key: 'entry', color: '#8B8D98' }],
+      won: { name_key: 'won' },
+      lost: { name_key: 'lost' },
+      lost_reasons: []
+    }
+  }.freeze
 
-    DEFAULT_KEY = 'blank'.freeze
-    WON_COLOR = '#2A7E3B'.freeze
-    LOST_COLOR = '#CE2C31'.freeze
+  DEFAULT_KEY = 'blank'.freeze
+  WON_COLOR = '#2A7E3B'.freeze
+  LOST_COLOR = '#CE2C31'.freeze
 
-    def self.fetch(key)
-      TEMPLATES.fetch(key_for(key))
-    end
+  def self.fetch(key)
+    TEMPLATES.fetch(key_for(key))
+  end
 
-    def self.key_for(key)
-      normalized_key = key.presence || DEFAULT_KEY
-      TEMPLATES.key?(normalized_key) ? normalized_key : DEFAULT_KEY
-    end
+  def self.key_for(key)
+    normalized_key = key.presence || DEFAULT_KEY
+    TEMPLATES.key?(normalized_key) ? normalized_key : DEFAULT_KEY
   end
 end
